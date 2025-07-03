a951a65707f1a04f976711fabb142284a6c2a00c
ChristianoBourguignon <christianombl@gmail.com>
2025-07-03 16:51:54 -0300

    feat: new methods and implements logics in SpecialCheck

--- ContaBancaria/src/main/java/br/com/contaBancaria/Controllers/ContaControllers.java
+++ ContaBancaria/src/main/java/br/com/contaBancaria/Controllers/ContaControllers.java
@@ -21,10 +21,27 @@ public class ContaControllers {
 
         this.conta.setSaldo(this.conta.getSaldo().add(saldo));
     }
-    public void RetirarSaldo(BigDecimal saldo) {
-        this.conta.setSaldo(this.conta.getSaldo().subtract(saldo));
-        if (!temSaldoPositivo()) {
+    public void RetirarSaldo(BigDecimal valorParaRetirar) {
+        BigDecimal saldoAtual = this.conta.getSaldo();
+
+        // Verificação quanto pode tirar do saldo
+        if (saldoAtual.compareTo(BigDecimal.ZERO) > 0) {
+            BigDecimal valorRetiradoDoSaldo = valorParaRetirar.min(saldoAtual);
+            this.conta.setSaldo(saldoAtual.subtract(valorRetiradoDoSaldo));
+            valorParaRetirar = valorParaRetirar.subtract(valorRetiradoDoSaldo);
+        }
+
+        // Se ainda tiver valor, terá que usar o cheque especial
+        if (valorParaRetirar.compareTo(BigDecimal.ZERO) > 0) {
             this.conta.setUsandoChequeEspecial(true);
+            retirarSaldoDoChequeEspecial(valorParaRetirar);
+        }
+    }
+
+    public void retirarSaldoDoChequeEspecial(BigDecimal saldo) {
+        if(this.conta.getUsandoChequeEspecial()) {
+            this.conta.setSaldo(new BigDecimal("0.00"));
+            this.conta.setChequeEspecial(this.conta.getChequeEspecial().subtract(saldo));
         }
     }
     public BigDecimal getSaldo() {
