41221c583a17489c7e8fb9be246c1fa354d9bf66
Christiano Bourguignon <christianombl@gmail.com>
2025-09-19 23:28:10 -0300

    feat: Button for change difficulty game

---
+++ Sudoku/src/br/com/sudoku/ui/custom/Button/ResetDifficultyGameButton.java
@@ -0,0 +1,10 @@
+package br.com.sudoku.ui.custom.Button;
+
+import javax.swing.*;
+
+public class ResetDifficultyGameButton extends JButton {
+    public ResetDifficultyGameButton() {
+        super("Mudar Dificuldade");
+        this.setBounds(350, 660, 200, 50);
+    }
+}
